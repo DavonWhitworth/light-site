078a6df77a863014dd21888edf1b1e6355eea42d
davon <davonwhitworth@gmail.com>
2021-12-18 22:00:09 -0800

    wip

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import "./styles.css"
+import "./Popup.js"
 
 
 function App() {
   return (
     <>
-      <button className="red" type="submit" onClick={handleRed}>Red</button>
-      <button className="orange" type="submit" onClick={handleOrange}>Orange</button>
-      <button className="yellow" type="submit" onClick={handleYellow}>Yellow</button>
-      <button className="green" type="submit" onClick={handleGreen}>Green</button>
-      <button className="blue" type="submit" onClick={handleBlue}>Blue</button>
-      <button className="purple" type="submit" onClick={handlePurple}>Purple</button>
-      <button className="customCol" type="submit" onClick={handleCustom}>Custom &#9660;</button>
-      <button className="reset" type="submit" onClick={() => window.location.reload(false)}>Reset</button>
-      <button className="info" type="submit" onClick={showInfo}>Info</button>
+      <button className="red" type="submit" onClick={handleRed}></button>
+      <button className="orange" type="submit" onClick={handleOrange}></button>
+      <button className="yellow" type="submit" onClick={handleYellow}></button>
+      <button className="green" type="submit" onClick={handleGreen}></button>
+      <button className="blue" type="submit" onClick={handleBlue}></button>
+      <button className="purple" type="submit" onClick={handlePurple}></button>
+      <button className="reset" type="submit" onClick={() => window.location.reload(false)}></button>
+      <button className="info" type="submit" onClick={showInfo}>&#63;</button>
 
     </>
   )
@@ -38,23 +38,16 @@ function App() {
     document.body.style.background = "purple";
   }
 
-
-  function handleCustom() {
-    document.body.style.backgroundColor = "DeepPink";
-    console.log("custom triggered");
-    /*set up custom color input, maybe try to enable 
-    the choice of a gradient, in all 4 direction up, # of colors: TBD*/
-  }
-
-  function showInfo() {
-    document.body.style.backgroundColor = "white";      /*display description*/
+  function showInfo() {      /*display description*/
     console.log("info triggered");
   }
 
 
   /*  
-    info display: insta, "use this to light your enviroment with you sceen..", 
-    "beginner project made by davon whitworth" (set name as hyperlink to open insta on new window)*/
+    info display: insta, "use this to light your enviroment 
+    with you sceen..", 
+    "beginner project made by davon whitworth" (set name as
+       hyperlink to open insta on new window)*/
 
 }
 
