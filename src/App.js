2305f8983419f92c5d1f67fcb3621439a0e15521
davon <davonwhitworth@gmail.com>
2021-12-23 19:17:56 -0800

    popup added with some styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./styles.css"
 import "./Popup.js"
 import React, { useState, useEffect } from 'react';
-
+import { Modal } from "./Popup";
 
 function App() {
+
+  const [showModal, setShowModal] = useState(false);
+
+  /*const openModal = () => {
+    setShowModal(true);
+  }*/
+
   return (
     <>
       <button className="red" type="submit" onClick={handleRed}></button>
@@ -13,8 +20,13 @@ function App() {
       <button className="blue" type="submit" onClick={handleBlue}></button>
       <button className="purple" type="submit" onClick={handlePurple}></button>
       <button className="reset" type="submit" onClick={() => window.location.reload(false)}></button>
-      <button className="info" type="submit" onClick={showInfo}>&#63;</button>
+      <button className="info" type="submit" onClick={() => setShowModal(true)}>?</button>
+      {showModal ? <Modal setShowModal={setShowModal} /> : null}
 
+
+      {/* <Popup open={isOpen} onClose={() => setIsOpen(false)}>
+        Fancy Modal
+      </Popup> */}
     </>
   )
 
@@ -38,12 +50,18 @@ function App() {
   function handlePurple() {
     document.body.style.background = "purple";
   }
-
-  function showInfo() {      /*display description*/
+  function handleInfo() {
     console.log("info triggered");
   }
 
 
+  //info drop down
+  /*
+    useEffect(() =>
+      console.log("fasdf")
+      , [showing])
+  
+  */
   /*  
     info display: insta, "use this to light your enviroment 
     with you sceen..", 
--- src/Popup.js
+++ src/Popup.js
@@ -1,9 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useRef } from "react";
+import ReactDom from "react-dom";
 
-export default function Popup() {
-    return (
-        <div>
 
-        </div>
-    )
-}
+export const Modal = ({ setShowModal }) => {
+    // close the modal when clicking outside the modal.
+    const modalRef = useRef();
+    const closeModal = (e) => {
+        if (e.target === modalRef.current) {
+            setShowModal(false);
+        }
+    };
+
+
+    //render the modal JSX in the portal div.
+    return ReactDom.createPortal(
+        <div className="container" ref={modalRef} onClick={closeModal}>
+            <div className="modal">
+                <h2>Light Site</h2>
+                <br /><br />
+                <p>This website can be used to illuminate your immidiate enviroment using your screen with the
+                    color of your choosing! Made by <a href="https://www.instagram.com/davon_whitworth/" target="_blank">Davon Whitworth</a>,
+                    inspired by <a href="https://keylit.app/" target="_blank">Keylit</a></p>
+                <button onClick={() => setShowModal(false)}>X</button>
+            </div>
+        </div>,
+        document.getElementById("portal")
+    );
+
+
+};
